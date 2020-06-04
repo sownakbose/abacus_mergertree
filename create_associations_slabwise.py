931e32b0a35995d7b7f9093503e4dfe1fa8ce885
Sownak Bose <sownak@Sownaks-MacBook-Pro.local>
2020-06-04 17:19:06 -0400

    Fixed indentation error

--- create_associations_slabwise.py
+++ create_associations_slabwise.py
@@ -599,9 +599,9 @@ for jj in range(len(steps)-1):
 	t_step_finish = time.time()
 	print("Total step time: %4.2fs"%(t_step_finish-t_step_start))
 	print("Total time spent building trees until now: %4.2fs"%(tree_build_time))
-        print("Total time spent querying trees until now: %4.2fs"%(tree_query_time))
+    print("Total time spent querying trees until now: %4.2fs"%(tree_query_time))
 	print("Total time spent reading until now: %4.2fs"%(read_time))
-        print("Total time spent writing until now: %4.2fs"%(write_time))
+    print("Total time spent writing until now: %4.2fs"%(write_time))
 	print("Total time spent matching haloes until now: %4.2fs"%(loop_time))
 
 	sys.stdout.flush()
