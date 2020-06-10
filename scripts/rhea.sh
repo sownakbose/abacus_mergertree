42e3390e355335a91e92f25a62274bd05257ec01
Lehman Garrison <lgarrison@cfa.harvard.edu>
2020-06-10 16:57:10 -0400

    Add scripts for parallel invocation on rhea

---
+++ scripts/rhea.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+SIMLISTFN=$1
+SIMNAME=$(awk -v NUM=$SLURM_PROCID '{if(NR==NUM+1) print $0}' ${SIMLISTFN})
+
+exec >> logs/${SIMNAME}.merger.out 2>&1
+
+#SIMNAME=${1:-AbacusSummit_highbase_c000_ph100}
+ABACUSSUMMIT=$PROJWORK/AbacusSummit
+OUTDIR=$ABACUSSUMMIT/merger
+
+NCORES=${SLURM_CPUS_PER_TASK:-16}
+
+cd $HOME/abacus_mergertree
+
+echo "Starting halo association for ${SIMNAME} on $(hostname) with ${NCORES} cores"
+
+#srun ./create_associations_slabwise.py -inputdir "$ABACUSSUMMIT" \
+#        -simname $SIMNAME \
+#        -num_chunks 16 \
+#        -num_cores $NCORES \
+#        -outputdir $OUTDIR \
+#        -num_epochs 8
+#        #-num_slabs_todo -1
+
+echo "Done halo associations."
